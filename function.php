7505d744a66b4fac49d959e10f0e1f2fc98c2cfe
= <=>
2025-09-02 12:39:12 +0700

    Function

---
+++ function.php
@@ -0,0 +1,13 @@
+<?php
+require_once('connection.php');
+
+function query($query) {
+  global $koneksi;
+  $result = mysqli_query($koneksi, $query);
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)){
+    $rows[] = $row;
+  }
+  return $rows;
+}
+?>
