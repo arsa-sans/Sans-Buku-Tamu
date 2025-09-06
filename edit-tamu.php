80394143eccb52d3e8bc88a9a69e64731edbde48
= <=>
2025-09-06 10:00:12 +0700

    “tambahkan fitur upload gambar di halaman edit tamu”

--- edit-tamu.php
+++ edit-tamu.php
@@ -35,8 +35,9 @@ include_once('templates/header.php');
     <div class="card-header py-3">
       <h6>Data Tamu</h6>
       <div class="card-body">
-        <form action="" method="post">
+        <form action="" method="post" enctype="multipart/form-data">
           <input type="hidden" name="id_tamu" id="id_tamu" value="<?= $id_tamu?>">
+          <input type="hidden" name="gambarLama" id="gambarLama" value="<?= $data['gambar']?>">
           <div class="form-group row">
             <label for="nama_tamu" class="col-sm-3 col-form-label">Nama Tamu</label>
             <div class="col-sm-8">
@@ -65,6 +66,14 @@ include_once('templates/header.php');
             <label for="kepentingan" class="col-sm-3 col-form-label">Kepentingan</label>
             <div class="col-sm-8">
               <input type="text" class="form-control" id="kepentingan" name="kepentingan" value="<?= $data['kepentingan']?>">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="gambar" class="col-sm-3 col-form-label">Gambar Foto</label>
+            <div class="col-sm-8">
+              <img src="assets/upload_gambar/<?= $data['gambar']?>" alt="" width="30%">
+              <input type="file" class="custom-file-input" id="gambar" name="gambar">
+              <label for="gambar" class="custom-file-label">Chosse file</label>
             </div>
           </div>
           <div class="form-group row">
